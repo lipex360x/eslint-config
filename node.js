1fdcb3066df4205ed19afe36d4f39071f322251f
Felipe Bambirra <lipex360@gmail.com>
2023-06-23 19:56:28 -0300

    project updated

--- node.js
+++ node.js
@@ -1,31 +1,30 @@
 module.exports = {
   env: {
-    es2021: true,
+    es2020: true,
     node: true,
   },
-  extends: ['standard', 'plugin:prettier/recommended'],
+
   parser: '@typescript-eslint/parser',
+
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+
+  plugins: ['@typescript-eslint', 'eslint-plugin-import-helpers'],
+
+  extends: ['standard', 'plugin:prettier/recommended'],
+
   rules: {
-    'prettier/prettier': [
+    'no-undef': 'off',
+    'no-useless-constructor': 'off',
+    'import-helpers/order-imports': [
       'error',
       {
-        printWidth: 80,
-        tabWidth: 2,
-        singleQuote: true,
-        trailingComma: 'all',
-        arrowParens: 'always',
-        semi: false,
+        newlinesBetween: 'always',
+        groups: ['absolute', 'module', '/^@/', 'index'],
+        alphabetize: { order: 'asc', ignoreCase: true },
       },
     ],
-  },
-  settings: {
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
-    },
   },
 }
