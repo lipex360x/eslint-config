d39d12ca860a637defd7ae532d21d1d7bf4afa1c
Felipe Bambirra <lipex360@gmail.com>
2023-06-23 19:55:18 -0300

    package updated

--- react.js
+++ react.js
@@ -1,59 +1,30 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
-    jest: true,
+    es2020: true,
+    node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'standard',
-    'plugin:prettier/recommended',
-  ],
+
   parser: '@typescript-eslint/parser',
+
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'jsx-a11y',
-    '@typescript-eslint'
-  ],
+
+  plugins: ['@typescript-eslint', 'eslint-plugin-import-helpers'],
+
+  extends: ['standard', 'plugin:prettier/recommended'],
+
   rules: {
-    'prettier/prettier': ["error", {
-      'printWidth': 80,
-      'tabWidth': 2,
-      'singleQuote': true,
-      'trailingComma': 'all',
-      'arrowParens': 'always',
-      'semi': false,
-      'endOfLine': 'auto',
-    }],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/alt-text': [
-      'warn',
+    'no-undef': 'off',
+    'no-useless-constructor': 'off',
+    'import-helpers/order-imports': [
+      'error',
       {
-        elements: ['img'],
-        img: ['Image'],
+        newlinesBetween: 'always',
+        groups: ['absolute', 'module', '/^@/', 'index'],
+        alphabetize: { order: 'asc', ignoreCase: true },
       },
     ],
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
-    },
-  }
 }
